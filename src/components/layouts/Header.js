2d1a4f760e6297f71694027a16bf6d2a343324dd
realhee <wlsgml5628@gmail.com>
2024-04-16 17:48:35 +0900

    Refactor: navigate 구문을 Link 태그로 변경

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,18 +1,22 @@
 // TODO : 재사용성으로 components로 분류했는데 공통 레이아웃 관련파일은 어떻게 분류?
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { ROUTE_PATH } from "../../constants/page";
 import HeaderTitle from "../HeaderTitle/HeaderTitle";
 import HeaderNavButton from "../HeaderNavButton/HeaderNavButton";
 
 export default function Header() {
-  const navigate = useNavigate();
-
   return (
     <nav className="nav flex justify-around">
-      <HeaderTitle title="Clean Code Shop" toPath={ROUTE_PATH.PRODUCT_LIST} />
+      <Link className="flex-center" to={ROUTE_PATH.PRODUCT_LIST}>
+        <h1 className="nav-title">CLEAN CODE SHOP</h1>
+      </Link>
       <div className="flex gap-15">
-        <HeaderNavButton title="장바구니" toPath={ROUTE_PATH.CART} />
-        <HeaderNavButton title="주문목록" toPath={ROUTE_PATH.ORDER} />
+        <Link className="flex-center" to={ROUTE_PATH.CART}>
+          <button className="nav-button">장바구니</button>
+        </Link>
+        <Link className="flex-center" to={ROUTE_PATH.ORDER}>
+          <button className="nav-button">주문목록</button>
+        </Link>
       </div>
     </nav>
   );
