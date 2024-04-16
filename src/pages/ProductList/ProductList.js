94ff8fe27a400a5989b83a2528e8247b3eb9e1eb
realhee <wlsgml5628@gmail.com>
2024-04-16 18:34:27 +0900

    Feat: Product data 없는 경우 message 렌더링

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -18,9 +18,9 @@ export default function ProductList() {
       {query.isLoading ? (
         <p>isLoading...</p>
       ) : (
-        query.data?.map((product) => (
+        query.data.length > 0? query.data.map((product) => (
           <Product key={product.id} product={product}></Product>
-        ))
+        )) : <h1 className="product-empty flex-center">등록된 상품이 없습니다.</h1>
       )}
     </section>
   );
