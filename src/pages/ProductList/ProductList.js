8bde5c4c80fc32c7d1c39e1a04b56aa493bfbcf3
realhee <wlsgml5628@gmail.com>
2024-04-16 18:48:39 +0900

    Feat: Product data 없는 경우 message 렌더링

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -4,6 +4,7 @@ import { getProducts } from "../../api/product";
 
 export default function ProductList() {
   // TODO : useQuery 에서 useEffect 필요한가?
+  // async await 처리?
   const query = useQuery({
     queryKey: ["products"],
     queryFn: getProducts,
@@ -18,9 +19,10 @@ export default function ProductList() {
       {query.isLoading ? (
         <p>isLoading...</p>
       ) : (
-        query.data.length > 0? query.data.map((product) => (
-          <Product key={product.id} product={product}></Product>
-        )) : <h1 className="product-empty flex-center">등록된 상품이 없습니다.</h1>
+        query.data.length > 0?
+          query.data.map((product) => (
+            <Product key={product.id} product={product}></Product>
+          )) : <h1 className="product-empty flex-center">등록된 상품이 없습니다.</h1>
       )}
     </section>
   );
